3e3ec0139694aa23a709f3514b691c560af41b67
userao <ostapenko2121@gmail.com>
2024-04-23 11:13:02 +0700

    changed api route for static

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use('/api', router);
-app.use(express.static(photosPath));
+app.use('/api/images', express.static(photosPath));
 
 async function startApp() {
   try {
